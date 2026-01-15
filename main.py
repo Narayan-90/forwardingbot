8806aaae5f65fd0fcc1adae5f559a70ce4bd1a92
Narayan-90 <Savageeditz18@gmail.com>
2026-01-15 01:38:49 -0500

    Refactor environment variable names and fix syntax

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-
 from telethon import TelegramClient, events
 from flask import Flask
 import threading
 import os
 
-api_id = int(os.environ["31114489"])
-api_hash = os.environ["990fc1c611ce074d3fb5b694fb88702c"]
-bot_token = os.environ["8110394902:AAHWQPCZMDWLOdzR5Q0c1J-76-UZsQyFz-M"]
+api_id = int(os.environ["API_ID"])
+api_hash = os.environ["API_HASH"]
+bot_token = os.environ["BOT_TOKEN"]
 
-source_channel = os.environ["https://t.me/hcmbd"]
-target_channel = os.environ["https://t.me/hcmdail"]
-app = Flask(__name__)
+source_channel = os.environ["SOURCE_CHANNEL"]
+target_channel = os.environ["TARGET_CHANNEL"]
+
+app = Flask(name)
 
 @app.route("/")
 def home():
@@ -25,6 +25,6 @@ def run_bot():
 
     client.run_until_disconnected()
 
-if __name__ == "main":
+if name == "main":
     threading.Thread(target=run_bot).start()
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
