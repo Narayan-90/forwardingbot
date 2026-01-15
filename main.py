14f4a5d78082504766fa468e3ab44016242ce5cd
Narayan-90 <Savageeditz18@gmail.com>
2026-01-15 01:36:22 -0500

    Integrate Flask to run bot with web server
    Added Flask integration to run a web server alongside the Telegram bot.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+
 from telethon import TelegramClient, events
+from flask import Flask
+import threading
 import os
 
 api_id = int(os.environ["31114489"])
@@ -7,11 +10,21 @@ bot_token = os.environ["8110394902:AAHWQPCZMDWLOdzR5Q0c1J-76-UZsQyFz-M"]
 
 source_channel = os.environ["https://t.me/hcmbd"]
 target_channel = os.environ["https://t.me/hcmdail"]
+app = Flask(__name__)
 
-client = TelegramClient("bot", api_id, api_hash).start(bot_token=bot_token)
+@app.route("/")
+def home():
+    return "Bot is running"
 
-@client.on(events.NewMessage(chats=source_channel))
-async def handler(event):
-    await client.send_message(target_channel, event.message)
+def run_bot():
+    client = TelegramClient("bot", api_id, api_hash).start(bot_token=bot_token)
 
-client.run_until_disconnected()
+    @client.on(events.NewMessage(chats=source_channel))
+    async def handler(event):
+        await client.send_message(target_channel, event.message)
+
+    client.run_until_disconnected()
+
+if __name__ == "main":
+    threading.Thread(target=run_bot).start()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
